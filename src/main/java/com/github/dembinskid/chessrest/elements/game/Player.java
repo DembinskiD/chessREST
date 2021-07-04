d1ff423aadf4f34f9e7469abd34138a0c7ec10f3
DembinskiD <dawid.dembinski@capgemini.com>
2021-07-04 22:29:00 +0200

    CHR-7 add listOfPiecesPerGame map

--- src/main/java/com/github/dembinskid/chessrest/elements/game/Player.java
+++ src/main/java/com/github/dembinskid/chessrest/elements/game/Player.java
@@ -1,5 +1,6 @@
 package com.github.dembinskid.chessrest.elements.game;
 
+import com.github.dembinskid.chessrest.elements.pieces.Piece;
 import lombok.Data;
 
 import java.util.*;
@@ -11,6 +12,7 @@ public class Player {
     private String emailAddress;
     private List<Game> playedGames; //must be list of game identificators to not to have all data about games
     private final Date playerCreated;
+    private Map<UUID, List<Piece>> listOfPiecesPerGame;
 
     public Player(String name, String emailAddress) {
         this.playerId = UUID.randomUUID();
@@ -18,5 +20,6 @@ public class Player {
         this.emailAddress = emailAddress;
         this.playedGames = new ArrayList<>();
         this.playerCreated = Calendar.getInstance().getTime();
+        this.listOfPiecesPerGame = new HashMap<>();
     }
 }
