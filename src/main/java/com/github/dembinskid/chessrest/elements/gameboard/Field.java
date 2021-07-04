6b2353b793b007c5026e84bf0bd6fb4de8436065
DembinskiD <dawid.dembinski@capgemini.com>
2021-07-04 20:58:25 +0200

    CHR-18 add constructor to Field

--- src/main/java/com/github/dembinskid/chessrest/elements/gameboard/Field.java
+++ src/main/java/com/github/dembinskid/chessrest/elements/gameboard/Field.java
@@ -11,12 +11,22 @@ public class Field implements Comparable<Field> {
     private boolean isTaken;
     private Piece piece;
 
-    public Field(int x, int y) {
-        this.x = x;
-        this.y = y;
+    public Field() {
         this.fieldColor = x + y % 2 == 0
                 ? FieldColor.BLACK : FieldColor.WHITE;
         this.isTaken = false;
+    }
+
+    public Field(char x, int y) {
+        new Field();
+        this.x = getValueByLiteral(x);
+        this.y = y;
+    }
+
+    public Field(int x, int y) {
+        new Field();
+        this.x = x;
+        this.y = y;
     }
 
     public String taken() {
@@ -30,6 +40,40 @@ public class Field implements Comparable<Field> {
         return isTaken() ?
                 String.format(formatTaken, piece.getPieceColor().getShortName(), piece.getPieceType().getShortName()) :
                 String.format(formatFree, getLiteral(x), y);
+    }
+
+    public static int getValueByLiteral(char x) {
+        int out;
+        switch(x) {
+            case 'A':
+                out = 1;
+                break;
+            case 'B':
+                out = 2;
+                break;
+            case 'C':
+                out = 3;
+                break;
+            case 'D':
+                out = 4;
+                break;
+            case 'E':
+                out = 5;
+                break;
+            case 'F':
+                out = 6;
+                break;
+            case 'G':
+                out = 7;
+                break;
+            case 'H':
+                out = 8;
+                break;
+            default:
+                out = 0;
+                break;
+        }
+        return out;
     }
 
     public static String getLiteral(int x) {
