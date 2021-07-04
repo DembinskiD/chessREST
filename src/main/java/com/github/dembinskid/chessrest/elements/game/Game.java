4e318894e637053100e70af1d1e1309073a0ae10
DembinskiD <dawid.dembinski@capgemini.com>
2021-07-04 22:09:06 +0200

    CHR-7 create base player class

--- src/main/java/com/github/dembinskid/chessrest/elements/game/Game.java
+++ src/main/java/com/github/dembinskid/chessrest/elements/game/Game.java
@@ -1,17 +1,19 @@
 package com.github.dembinskid.chessrest.elements.game;
 
+import java.util.Date;
 import java.util.List;
+import java.util.UUID;
 
 public class Game {
+    UUID id;
+    String gameDescription;
     Player firstPlayer;
     Player secondPlayer;
     List<Turn> turnList;
     List<Movement> movementList; //list of moves done already
     List<Movement> possibleMoves; //list of possible moves in currentTurn
     Turn currentTurn;
-    long startTime; //posix time
-
-
+    Date startTime;
 
 
 }
--- src/main/java/com/github/dembinskid/chessrest/elements/game/Player.java
+++ src/main/java/com/github/dembinskid/chessrest/elements/game/Player.java
@@ -1,5 +1,20 @@
 package com.github.dembinskid.chessrest.elements.game;
 
+import lombok.Data;
+
+import java.util.*;
+
+@Data
 public class Player {
-    //todo add players
+    private UUID playerId;
+    private String name;
+    private List<Game> playedGames; //must be list of game identificators to not to have all data about games
+    private final Date playerCreated;
+
+    public Player(String name) {
+        this.playerId = UUID.randomUUID();
+        this.name = name;
+        this.playedGames = new ArrayList<>();
+        this.playerCreated = Calendar.getInstance().getTime();
+    }
 }
